b58a5996d1b57acc433b3fd246c4bc1c6364a29a
duck7000 <ed@riethorst.net>
2026-01-29 17:46:54 +0100

    Tv class simplified watchproviders, no user changes

--- src/Tmdb/Tv.php
+++ src/Tmdb/Tv.php
@@ -385,64 +385,18 @@ class Tv extends MdbBase
         // Watch providers for this movie
         if (!empty($data->{"watch/providers"}->results)) {
             $watchProviderResults = (array) $data->{"watch/providers"}->results;
-            foreach ($watchProviderResults as $country => $providerItems) {
-                $buy = array();
-                $rent = array();
-                $flatrate = array();
-                // buy
-                if (isset($providerItems->buy) &&
-                    is_array($providerItems->buy) &&
-                    count($providerItems->buy) > 0
-                   )
-                {
-                    foreach ($providerItems->buy as $provider) {
-                        $buy[] = array(
-                            'providerId' => isset($provider->provider_id) ? $provider->provider_id : null,
-                            'providerName' => isset($provider->provider_name) ? $provider->provider_name : null,
-                            'imgLogoPath' => isset($provider->logo_path) ? $this->config->baseImageUrl . '/' .
-                                                                           $this->config->logoImageSize .
-                                                                           $provider->logo_path : null
-                        );
-                    }
-                }
-                //rent
-                if (isset($providerItems->rent) &&
-                    is_array($providerItems->rent) &&
-                    count($providerItems->rent) > 0
-                   )
-                {
-                    foreach ($providerItems->rent as $providerRent) {
-                        $rent[] = array(
-                            'providerId' => isset($providerRent->provider_id) ? $providerRent->provider_id : null,
-                            'providerName' => isset($providerRent->provider_name) ? $providerRent->provider_name : null,
-                            'imgLogoPath' => isset($providerRent->logo_path) ? $this->config->baseImageUrl . '/' .
+            foreach ($watchProviderResults as $watchindexKey => $providerItems) {
+                foreach ($providerItems as $itemsKey => $provider) {
+                    foreach ($provider as $providerData) {
+                        $this->watchProviders[$watchindexKey][$itemsKey][] = array(
+                            'providerId' => isset($providerData->provider_id) ? $providerData->provider_id : null,
+                            'providerName' => isset($providerData->provider_name) ? $providerData->provider_name : null,
+                            'imgLogoPath' => isset($providerData->logo_path) ? $this->config->baseImageUrl . '/' .
                                                                                $this->config->logoImageSize .
-                                                                               $providerRent->logo_path : null
+                                                                               $providerData->logo_path : null
                         );
                     }
                 }
-                // flatrate (stream)
-                if (isset($providerItems->flatrate) &&
-                    is_array($providerItems->flatrate) &&
-                    count($providerItems->flatrate) > 0
-                   )
-                {
-                    foreach ($providerItems->flatrate as $providerFlatrate) {
-                        $flatrate[] = array(
-                            'providerId' => isset($providerFlatrate->provider_id) ? $providerFlatrate->provider_id : null,
-                            'providerName' => isset($providerFlatrate->provider_name) ? $providerFlatrate->provider_name : null,
-                            'imgLogoPath' => isset($providerFlatrate->logo_path) ? $this->config->baseImageUrl . '/' .
-                                                                                   $this->config->logoImageSize .
-                                                                                   $providerFlatrate->logo_path : null
-                        );
-                    }
-                }
-                $this->watchProviders[$country] = array(
-                    'link' => isset($providerItems->link) ? $providerItems->link : null,
-                    'buy' => $buy,
-                    'rent' => $rent,
-                    'flatrate' => $flatrate
-                );
             }
         }
         // reviews
